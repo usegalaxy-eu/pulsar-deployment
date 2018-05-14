b4e7f41e6ffc7f481d67636369201d98805de03f
Helena Rasche <hxr@hx42.org>
2018-05-14 16:45:28 +0200

    split conf

---
+++ vars.tf
@@ -0,0 +1,20 @@
+variable "image" {
+  default = "vggp-v29-3472699b14b5-j24-passordless"
+}
+
+variable "key_pair" {
+  default = "cloud2"
+}
+
+variable "secgroups" {
+	default = [
+		"ingress-public",
+		"egress"
+	]
+}
+
+variable "network" {
+	default = [
+		{ name = "galaxy-net" }
+	]
+}
