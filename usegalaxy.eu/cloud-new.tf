845014193db087c25de2c981ddbc502909f1182c
Helena Rasche <hxr@hx42.org>
2018-07-11 14:40:59 +0200

    cloud conf ohne passwort

---
+++ usegalaxy.eu/cloud-new.tf
@@ -0,0 +1,7 @@
+provider "openstack" {
+  user_name = "er1025@uni-freiburg.de"
+  tenant_id = "36d6d9420bb04403b55fb21052efe4be"
+  auth_url  = "https://idm01.bw-cloud.org:5000/v3"
+  region    = "Freiburg"
+  domain_id = "default"
+}
