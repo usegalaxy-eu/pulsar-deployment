15cbc2c7ba626bfbd49a9b8f7b339281e5a49cbd
Helena Rasche <hxr@hx42.org>
2018-08-31 13:16:29 +0200

    stuff

--- usegalaxy.eu/instance_debian-test.tf
+++ usegalaxy.eu/instance_debian-test.tf
@@ -1,6 +1,6 @@
 resource "openstack_compute_instance_v2" "debian" {
-  name            = "debian-test"
-  image_name      = "Ubuntu 18.04"
+  name            = "test"
+  image_name      = "vggp-v30-j56-c5ccc6e703bf-asdf3"
   flavor_name     = "m1.xlarge"
   key_pair        = "cloud2"
   security_groups = ["egress", "ufr-ssh"]
