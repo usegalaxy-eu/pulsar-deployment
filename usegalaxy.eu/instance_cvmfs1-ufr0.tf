6bf937eba5186b41c77082faad5dda75dba1ba81
Helena Rasche <hxr@hx42.org>
2018-09-18 15:45:52 +0200

    move outside

--- usegalaxy.eu/instance_cvmfs1-ufr0.tf
+++ usegalaxy.eu/instance_cvmfs1-ufr0.tf
@@ -1,5 +1,5 @@
 resource "openstack_compute_instance_v2" "cvmfs1-ufr0-usegalaxy" {
-  name            = "cvmfs1-ufr0.usegalaxy.eu"
+  name            = "cvmfs1-ufr0.galaxyproject.eu"
   image_name      = "${var.centos_image}"
   flavor_name     = "m1.small"
   key_pair        = "cloud2"
@@ -15,8 +15,8 @@ resource "openstack_compute_instance_v2" "cvmfs1-ufr0-usegalaxy" {
 }
 
 resource "aws_route53_record" "cvmfs1-ufr0-usegalaxy" {
-  zone_id = "${var.zone_usegalaxy_eu}"
-  name    = "cvmfs1-ufr0.usegalaxy.eu"
+  zone_id = "${var.zone_galaxyproject_eu}"
+  name    = "cvmfs1-ufr0.galaxyproject.eu"
   type    = "A"
   ttl     = "7200"
   records = ["${openstack_compute_instance_v2.cvmfs1-ufr0-usegalaxy.access_ip_v4}"]
--- usegalaxy.eu/instance_influxdb.tf
+++ usegalaxy.eu/instance_influxdb.tf
@@ -21,19 +21,10 @@ resource "openstack_compute_volume_attach_v2" "influxdb-va" {
   volume_id   = "${openstack_blockstorage_volume_v2.influxdb-data.id}"
 }
 
-# CNAME since everything should go through proxy
-resource "aws_route53_record" "influxdb-usegalaxy" {
-  zone_id = "${var.zone_galaxyproject_eu}"
-  name    = "influxdb.galaxyproject.eu"
-  type    = "CNAME"
-  ttl     = "7200"
-  records = ["proxy.galaxyproject.eu"]
-}
-
 # But an internal record to permit SSHing until we find a nice solution for that.
 resource "aws_route53_record" "influxdb-usegalaxy-internal" {
   zone_id = "${var.zone_galaxyproject_eu}"
-  name    = "influxdb.internal.galaxyproject.eu"
+  name    = "influxdb.galaxyproject.eu"
   type    = "A"
   ttl     = "7200"
   records = ["${openstack_compute_instance_v2.influxdb-usegalaxy.access_ip_v4}"]
