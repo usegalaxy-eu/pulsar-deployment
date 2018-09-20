0dfc3e4631a441d5832b9ba1a3dabba66136efd1
Helena Rasche <hxr@hx42.org>
2018-09-20 17:50:52 +0200

    fix

--- usegalaxy.eu/instance_jenkins-worker.tf
+++ usegalaxy.eu/instance_jenkins-worker.tf
@@ -3,7 +3,7 @@ resource "openstack_compute_instance_v2" "jenkins-workers" {
   image_name      = "${var.jenkins_image}"
   flavor_name     = "m1.xlarge"
   key_pair        = "build-usegalaxy-eu"
-  security_groups = ["egress", "ufr-ssh"]
+  security_groups = ["public"]
   count           = 2
 
   network {
