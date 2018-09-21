05865fafdf0bf993a35dfe3944db431cf3e39a0c
Helena Rasche <hxr@hx42.org>
2018-09-21 10:49:41 +0200

    include script for this

--- usegalaxy.eu/add-training.sh
+++ usegalaxy.eu/add-training.sh
@@ -18,3 +18,26 @@ cat training-template.txt | \
     sed "s/VM_COUNT/${vm_count}/g" > $output
 
 #echo "Done: instance_training-${training_identifier}.tf"
+
+
+vm_cpu=$(echo $vm_size | sed 's/[^0-9]/ /g' | awk '{print $1}')
+vm_mem=$(echo $vm_size | sed 's/[^0-9]/ /g' | awk '{print $2}')
+
+
+echo "
+Subject: Training Infrastructure Request: Approved
+
+Hello,
+
+We're happy to provide resources for your training. Based on your requested
+training we have allocated ${vm_count} server(s), each with ${vm_cpu} Cores and ${vm_mem} GB of RAM.
+This should be sufficient for your purposes. If you find that it is not, please contact us
+and we can update that at any time.
+
+On the day of your training, please ask your users to go to the following URL:
+https://usegalaxy.eu/join-training/${training_identifier}
+
+They will be added to the training group and put into a private queue which
+should be a bit faster than our regular queue.
+
+Please let us know if you have any questions!"
