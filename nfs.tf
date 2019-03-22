a39d3de6fa0085e77c9f354e9659364f2da4017d
Gianmauro Cuccuru <gmauro@gmail.com>
2019-03-22 14:36:04 +0100

    creates and mounts the filesystem for the NFS share.

--- nfs.tf
+++ nfs.tf
@@ -1,12 +1,48 @@
 resource "openstack_compute_instance_v2" "nfs-server" {
   name            = "${var.name_prefix}nfs${var.name_suffix}"
+  image_id        = "${data.openstack_images_image_v2.vgcn-image.id}"
   flavor_name     = "${var.flavors[1]}"
-  image_name      = "${var.image}"
   key_pair        = "${openstack_compute_keypair_v2.my-cloud-key.name}"
   security_groups = "${var.secgroups}"
   network         = "${var.network}"
 
-  user_data = <<-EOF
+  block_device {
+    uuid                  = "${openstack_blockstorage_volume_v2.volume_nfs_data.id}"
+    source_type           = "image"
+    destination_type      = "local"
+    boot_index            = 0
+    delete_on_termination = true
+  }
+
+  block_device {   
+    uuid                  = "${openstack_blockstorage_volume_v2.volume_nfs_data.id}"
+    source_type           = "volume"
+    destination_type      = "volume"
+    boot_index            = -1
+    delete_on_termination = true
+  }
+
+  user_data = "${data.template_cloudinit_config.nfs-share.rendered}"
+}
+
+
+resource "openstack_blockstorage_volume_v2" "volume_nfs_data" {
+  name = "${var.name_prefix}volume_nfs_data"
+  size = 3
+}
+
+data "template_cloudinit_config" "nfs-share" {
+  gzip          = true
+  base64_encode = true
+
+  part {
+    content_type = "text/x-shellscript"
+    content      = "${file("${path.module}/files/create_share.sh")}"
+  }
+
+  part {
+    content_type = "text/cloud-config"
+    content      = <<-EOF
     #cloud-config
     write_files:
     - content: |
@@ -16,10 +52,11 @@ resource "openstack_compute_instance_v2" "nfs-server" {
       permissions: '0644'
 
     runcmd:
-     - [ mkdir, -p, /data/share ]
-     - [ chown, "centos:centos", -R, /data/share ]
      - [ systemctl, enable, nfs-server ]
      - [ systemctl, start, nfs-server ]
      - [ exportfs, -avr ]
   EOF
+  }
+
+
 }
