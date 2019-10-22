f0c81c484f909249e8d8e38aca81354c19743222
Gianmauro Cuccuru <gmauro@gmail.com>
2019-10-22 14:46:42 +0200

    update vgcn image

--- tf/vars.tf
+++ tf/vars.tf
@@ -23,8 +23,8 @@ variable "gpu_node_count" {
 variable "image" {
   type = "map"
   default = {
-    "name" = "vggp-v31-j128-0c5f1226c21c-master"
-    "image_source_url" = "https://usegalaxy.eu/static/vgcn/vggp-v31-j128-0c5f1226c21c-master.raw"
+    "name" = "vggp-v31-j132-4ab83d5ffde9-master"
+    "image_source_url" = "https://usegalaxy.eu/static/vgcn/vggp-v31-j132-4ab83d5ffde9-master.raw"
     "container_format" = "bare"
     "disk_format" = "raw"
    }
