38b50ca5bfe5452cbcef8a30b74c3290dbd1146e
Mira <86979912+mira-miracoli@users.noreply.github.com>
2023-03-02 13:53:44 +0100

    nl

--- tf/image.tf
+++ tf/image.tf
@@ -2,6 +2,7 @@ data "openstack_images_image_v2" "vgcn-image" {
   //name = "${var.image["name"]}"
   most_recent = true
 }
+
 data "openstack_images_image_v2" "vgcn-image-gpu" {
   //name = "${var.image["name"]}"
   most_recent = true
