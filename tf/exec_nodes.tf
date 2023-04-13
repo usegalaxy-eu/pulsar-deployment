835d2581dbfcc4631b7e4be6a61e155577923420
Sebastian Luna-Valero <sebastian.luna.valero@gmail.com>
2023-04-13 18:34:45 +0200

    condor config not needed after https://github.com/usegalaxy-eu/vgcn/pull/56

--- tf/exec_nodes.tf
+++ tf/exec_nodes.tf
@@ -15,32 +15,6 @@ resource "openstack_compute_instance_v2" "exec-node" {
     #cloud-config
     write_files:
     - content: |
-        CONDOR_HOST = ${openstack_compute_instance_v2.central-manager.network.1.fixed_ip_v4}
-        ALLOW_WRITE = *
-        ALLOW_READ = $(ALLOW_WRITE)
-        ALLOW_ADMINISTRATOR = *
-        ALLOW_NEGOTIATOR = $(ALLOW_ADMINISTRATOR)
-        ALLOW_CONFIG = $(ALLOW_ADMINISTRATOR)
-        ALLOW_DAEMON = $(ALLOW_ADMINISTRATOR)
-        ALLOW_OWNER = $(ALLOW_ADMINISTRATOR)
-        ALLOW_CLIENT = *
-        DAEMON_LIST = MASTER, SCHEDD, STARTD
-        FILESYSTEM_DOMAIN = vgcn
-        UID_DOMAIN = vgcn
-        TRUST_UID_DOMAIN = True
-        SOFT_UID_DOMAIN = True
-        # run with partitionable slots
-        CLAIM_PARTITIONABLE_LEFTOVERS = True
-        NUM_SLOTS = 1
-        NUM_SLOTS_TYPE_1 = 1
-        SLOT_TYPE_1 = 100%
-        SLOT_TYPE_1_PARTITIONABLE = True
-        ALLOW_PSLOT_PREEMPTION = False
-        STARTD.PROPORTIONAL_SWAP_ASSIGNMENT = True
-      owner: root:root
-      path: /etc/condor/condor_config.local
-      permissions: '0644'
-    - content: |
         /data           /etc/auto.data          nfsvers=3
       owner: root:root
       path: /etc/auto.master.d/data.autofs
--- tf/main.tf
+++ tf/main.tf
@@ -58,19 +58,6 @@ resource "openstack_compute_instance_v2" "central-manager" {
       path: /etc/ssh/vgcn.key
       permission: '0644'
     - content: |
-        CONDOR_HOST = localhost
-        ALLOW_WRITE = *
-        ALLOW_READ = $(ALLOW_WRITE)
-        ALLOW_NEGOTIATOR = $(ALLOW_WRITE)
-        DAEMON_LIST = COLLECTOR, MASTER, NEGOTIATOR, SCHEDD
-        FILESYSTEM_DOMAIN = vgcn
-        UID_DOMAIN = vgcn
-        TRUST_UID_DOMAIN = True
-        SOFT_UID_DOMAIN = True
-      owner: root:root
-      path: /etc/condor/condor_config.local
-      permissions: '0644'
-    - content: |
         /data           /etc/auto.data          nfsvers=3
       owner: root:root
       path: /etc/auto.master.d/data.autofs
