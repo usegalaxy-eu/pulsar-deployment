e42f0ede9473bd49157d3d2e600c04d0460b6953
gm-ds <128259668+gm-ds@users.noreply.github.com>
2025-05-14 08:30:26 +0000

    data images debug

--- tf/image.tf
+++ tf/image.tf
@@ -1,16 +1,16 @@
 // Since th available format of the VGCN cannot be used in Oracle
 // images are to be processed and uploaded manually for the time being
 
-data "oci_core_image" "vgcn_image" {
+data "oci_core_images" "vgcn_image" {
   compartment_id           = var.oracle_vars.compartment_id
-  image_id                 = var.image.ocid
+  display_name             = var.image.ocid
 
 }
 
 // Upload virtual machine GPU image via API
 // comment this block if the GPU image is already available or if you upload it via the dashboard interface
-data "oci_core_image" "vgcn_image_gpu" {
+data "oci_core_images" "vgcn_image_gpu" {
   compartment_id           = var.oracle_vars.compartment_id
-  image_id                 = var.gpu_image.ocid
+  display_name             = var.gpu_image.ocid
 
 }
