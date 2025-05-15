c5c593208649c70bf72c0902924d104e6e05230f
gm-ds <128259668+gm-ds@users.noreply.github.com>
2025-05-15 15:33:10 +0000

    cloud-init refactor

--- tf/gpu_nodes.tf
+++ tf/gpu_nodes.tf
@@ -1,3 +1,14 @@
+locals {
+  gpu_exec_user_data = templatefile(
+    "${path.module}/cloud_init_templates/gpu_exec_cloud_init.yaml.tftpl",
+    {
+      nfs_ip = oci_core_instance.nfs_server.private_ip
+      cm_ip       = oci_core_instance.central_manager.private_ip
+      condor_pass = var.condor_pass
+    }
+  )
+}
+
 resource "oci_core_instance" "gpu-node" {
 
   count               = var.gpu_node_count
@@ -32,88 +43,7 @@ resource "oci_core_instance" "gpu-node" {
 
   metadata = {
   ssh_authorized_keys = local.ssh_public_key
-  user_data = base64encode(<<EOF
-    #cloud-config
-    system_info:
-      default_user:
-        name: centos
-        gecos: RHEL Cloud User
-        groups: [wheel, adm, systemd-journal]
-        sudo: ["ALL=(ALL) NOPASSWD:ALL"]
-        shell: /bin/bash
-      distro: rhel
-      paths:
-        cloud_dir: /var/lib/cloud
-        templates_dir: /etc/cloud/templates
-      ssh_svcname: sshd
-    write_files:
-    - content: |
-        ALLOW_WRITE = *
-        ALLOW_READ = $(ALLOW_WRITE)
-        ALLOW_ADMINISTRATOR = *
-        ALLOW_NEGOTIATOR = $(ALLOW_ADMINISTRATOR)
-        ALLOW_CONFIG = $(ALLOW_ADMINISTRATOR)
-        ALLOW_DAEMON = $(ALLOW_ADMINISTRATOR)
-        ALLOW_OWNER = $(ALLOW_ADMINISTRATOR)
-        ALLOW_CLIENT = *
-        DAEMON_LIST = MASTER, SCHEDD, STARTD
-        FILESYSTEM_DOMAIN = vgcn
-        UID_DOMAIN = vgcn
-        TRUST_UID_DOMAIN = True
-        SOFT_UID_DOMAIN = True
-        # Advertise the GPUs
-        use feature : GPUs
-        GPU_DISCOVERY_EXTRA = -extra
-        # run with partitionable slots
-        CLAIM_PARTITIONABLE_LEFTOVERS = True
-        NUM_SLOTS = 1
-        NUM_SLOTS_TYPE_1 = 1
-        SLOT_TYPE_1 = 100%
-        SLOT_TYPE_1_PARTITIONABLE = True
-        ALLOW_PSLOT_PREEMPTION = False
-        STARTD.PROPORTIONAL_SWAP_ASSIGNMENT = True
-      owner: root:root
-      path: /etc/condor/condor_config.local
-      permissions: '0644'
-    - content: |
-        /data           /etc/auto.data          nfsvers=3
-      owner: root:root
-      path: /etc/auto.master.d/data.autofs
-      permissions: '0644'
-    - content: |
-        share  -rw,hard,intr,nosuid,quota  ${oci_core_instance.nfs_server.private_ip}:/data/share
-      owner: root:root
-      path: /etc/auto.data
-      permissions: '0644'
-    - content: |
-        ---
-        - name: Install HTCondor Central Manager on Pulsar
-          become: yes
-          hosts: all
-          connection: local
-          roles:
-            - name: usegalaxy_eu.htcondor
-              vars:
-                condor_role: execute
-                condor_copy_template: false
-                condor_host: ${oci_core_instance.central_manager.private_ip}
-                condor_password: ${var.condor_pass}
-
-      owner: centos:centos
-      path: /home/centos/condor.yml
-      permissions: '0644'
-    packages: 
-      - ansible
-    runcmd:
-      - [ sh, -xc, "sed -i 's|nameserver 10.0.2.3||g' /etc/resolv.conf" ]
-      - [ sh, -xc, "sed -i 's|localhost.localdomain|$(hostname -f)|g' /etc/telegraf/telegraf.conf" ]
-      - systemctl restart telegraf
-      - [ python3, -m, pip, install, ansible ]
-      - [ ansible-galaxy, install, -p, /home/centos/roles, usegalaxy_eu.htcondor ]
-      - [ ansible-playbook, -i, 'localhost,', /home/centos/condor.yml]
-      - systemctl start condor
-  EOF
-  )
+  user_data = base64encode(local.gpu_exec_user_data)
 }
 }
 
--- tf/nfs.tf
+++ tf/nfs.tf
@@ -1,3 +1,8 @@
+# Cloud-init template to configure NFS
+locals {
+  nfs_user_data = file("${path.module}/files/nfs_cloud_init.yaml") 
+}
+
 # Create a block volume for NFS data
 resource "oci_core_volume" "volume_nfs_data" {
   availability_domain = var.oracle_vars.availability_domain
@@ -13,50 +18,13 @@ resource "oci_core_volume_attachment" "nfs_data_attachment" {
   volume_id       = oci_core_volume.volume_nfs_data.id
 }
 
-# Cloud-init template to configure NFS
-locals {
-  create_share = file("${path.module}/files/create_share.sh")
-}
-
-data "template_cloudinit_config" "nfs_share" {
-  gzip          = true
-  base64_encode = true
-
-  part {
-    content_type = "text/cloud-config"
-    content      = <<EOF
-    #cloud-config
-    write_files:
-    - content: |
-        /data/share *(rw,sync)
-      owner: root:root
-      path: /etc/exports
-      permissions: '0644'
-    - content: |
-        ${local.create_share}
-      owner: root:root
-      path: /etc/create_share.sh
-      permissions: '0754'
-
-    runcmd:
-     - [ sh, -xc, "sed -i 's|nameserver 10.0.2.3||g' /etc/resolv.conf" ]
-     - [ firewall-cmd, --permanent, --add-port=2049/tcp ]
-     - [ firewall-cmd, --reload ]
-     - [ bash, /etc/create_share.sh ]
-     - [ systemctl, enable, nfs-server ]
-     - [ systemctl, start, nfs-server ]
-     - [ exportfs, -avr ]
-    EOF
-  }
-}
-
 # Create the NFS server instance
 resource "oci_core_instance" "nfs_server" {
   availability_domain = var.oracle_vars.availability_domain
   compartment_id      = var.oracle_vars.compartment_id
   display_name        = "${var.name_prefix}nfs${var.name_suffix}"
   shape               = var.shapes["nfs-server"].shape
-  depends_on          = [ oci_core_subnet.private_subnet, oci_core_volume.volume_nfs_data ]
+  depends_on          = [ oci_core_volume.volume_nfs_data ]
  
  shape_config {
     ocpus = var.shapes["nfs-server"].ocpus
@@ -87,7 +55,7 @@ resource "oci_core_instance" "nfs_server" {
   # SSH key configuration
   metadata = {
     ssh_authorized_keys = local.ssh_public_key
-    user_data           = data.template_cloudinit_config.nfs_share.rendered
+    user_data           = base64encode(local.nfs_user_data)
   }
   
   preserve_boot_volume = false
